1a01f394077029b21893d8656dd9ebc7475428dc
Ishan Alone <ishan.alone@gmail.com>
2016-10-06 19:14:38 +0530

    Initial Commit

---
+++ Sydra/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Sydra
+//
+//  Created by Ishan Alone on 06/10/16.
+//  Copyright © 2016 Ishan Alone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SydraTests/SydraTests.m
@@ -0,0 +1,39 @@
+//
+//  SydraTests.m
+//  SydraTests
+//
+//  Created by Ishan Alone on 06/10/16.
+//  Copyright © 2016 Ishan Alone. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SydraTests : XCTestCase
+
+@end
+
+@implementation SydraTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
